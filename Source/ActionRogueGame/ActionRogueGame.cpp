7eca6a1042be1c40a8b93ce94fa4a41f8f6f7bb0
Shizalie <shizalie@gmail.com>
2023-04-07 21:47:20 +0300

    Added ARCharacter and Setup Input
    -Added Character class
    -Added MoveForward and Turn Input

---
+++ Source/ActionRogueGame/ActionRogueGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActionRogueGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionRogueGame, "ActionRogueGame" );
---
+++ Source/ActionRogueGame/ActionRogueGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRogueGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUEGAME_API AActionRogueGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
